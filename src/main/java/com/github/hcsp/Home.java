82b7a3495d6c08334abaa967fcc0e76d70d87563
Bo Zhang <zhangbodut@gmail.com>
2020-05-09 20:32:40 +0800

    Update Home.java

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -2,8 +2,12 @@ package com.github.hcsp;
 
 import com.github.hcsp.pet1.Cat;
 import com.github.hcsp.pet2.Dog;
+import java.util.Map;
 
 public class Home {
     Cat cat;
     Dog dog;
+    static {
+        int i=1;
+    }
 }
