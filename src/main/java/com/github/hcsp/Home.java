bdfeb86c05179800048fdde6ede619517d146768
GJC <599953528@qq.com>
2024-09-04 21:22:22 +0800

    创建两个cat类

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -1,9 +1,9 @@
 package com.github.hcsp;
 
-import com.github.hcsp.pet1.Cat;
-import com.github.hcsp.pet2.Dog;
+//import com.github.hcsp.pet1.Cat;
+import com.github.hcsp.pet2.Cat;
 
 public class Home {
-    Cat cat;
-    Dog dog;
+    Cat cat1;
+    com.github.hcsp.pet2.Cat cat2;
 }
--- src/test/java/com/github/hcsp/HomeTest.java
+++ src/test/java/com/github/hcsp/HomeTest.java
@@ -1,14 +1,13 @@
 package com.github.hcsp;
 
-import com.github.hcsp.pet1.Cat;
-import com.github.hcsp.pet2.Dog;
+import com.github.hcsp.pet2.Cat;
 import org.junit.jupiter.api.Test;
 
 public class HomeTest {
     @Test
     public void homeHasCatAndDog() {
         Home home = new Home();
-        home.cat = new Cat();
-        home.dog = new Dog();
+        home.cat1 = new Cat();
+        home.cat2 = new Cat();
     }
 }
